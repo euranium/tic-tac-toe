1ded6278aeb44d898f021deaca2c168c73ac1de9
euranium <mountrainier13@gmail.com>
2014-11-19 13:19:31 -0800

    added python compile script

---
+++ compile.py
@@ -0,0 +1,8 @@
+import os
+os.system('javac Board.java')
+os.system('javac Comp.java')
+os.system('javac Game.java')
+os.system('javac Player.java')
+os.system('javac compLogic.java')
+os.system('javac playerIO.java')
+os.system('javac tic.java')
